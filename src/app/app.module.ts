8fa89fd225f0d4ed4b5c86be3f80c21f84e8c907
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2022-05-19 21:04:33 +0300

    fix: refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,12 +21,6 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     DragDropModule,
     SharedModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
